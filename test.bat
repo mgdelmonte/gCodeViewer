902e8f6eb200d22ec2b362581809a156d57a994d
mgdelmonte <mgdelmonte@gmail.com>
2019-03-16 23:18:51 -0400

    nit

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=. --allow-file-access-from-files file:///%cd%/index.html
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=chrome --allow-file-access-from-files file:///%cd%/index.html
